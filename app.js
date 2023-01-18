2258912a107e2a146bb35924dfd759f326c54f20
ArpanManna98 <arpanmanna98@gmail.com>
2023-01-18 13:03:04 +0530

    Authentication done

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const ejs = require("ejs");
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
 let fs = require('fs');
 const bodyParser = require('body-parser')
 const { join } = require('path');
@@ -12,28 +15,51 @@ app.use(express.static(join(__dirname, 'public')));
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//serving public file
+// app.use(express.static(__dirname));
+app.use(cookieParser());
+app.set("view engine","ejs");
+
+const PORT = 8001;
+
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false
+}));
+
+
+// a variable to save a session
+let session;
+
 
 app.get('/', function (req, response) {
-    fs.readFile('./html/loginpage.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('Whoops! File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
+    session = req.session;
+    if (session.userEmail) {
+        fs.readFile('./html/welcome.html', null, function (error, data) {
+            if (error) {
+                response.writeHead(404);
+                respone.write('Whoops! File not found!');
+            } else {
+                response.write(data);
+            }
+            response.end();
+        });
+    } else
+        response.sendFile('./html/loginpage.html', { root: __dirname })
+
+
 })
+
+
 app.get('/welcome', function (req, response) {
-    fs.readFile('./html/welcome.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('Whoops! File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
+    session = req.session;
+    response.render("welcome",{userEmail:session.userEmail})
 })
 app.post('/register', function (req, res) {
     let nameVar = req.body.namezzzz;
@@ -79,13 +105,16 @@ app.post('/login', async (req, res) => {
                 console.log(error);
             } else {
                 console.log(results);
-                if(results.length<1){
-                    return  res.send({ msg: 'Incorrect Email and Password', loginstatus: false });
+                if (results.length < 1) {
+                    return res.send({ msg: 'Incorrect Email and Password', loginstatus: false });
                 }
                 const { email, password } = results[0]
                 let uid = email;
                 let pd = password;
                 if (useremailyyy == uid && userpwdyyy == pd) {
+                    session = req.session;
+                    session.userEmail = useremailyyy;
+                    console.log(req.session)
                     res.send({ msg: 'Succefully Loggin', loginstatus: true });
                 }
                 else if (useremailyyy == uid && userpwdyyy != pd) {
